7091f7226bbc86aee7170e78f7d5c3d0a6813cc2
Durgesh Maheshwari <dur2122soni@gmail.com>
2026-07-24 18:09:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -28,21 +28,20 @@ const resetGame = () => {
 boxes.forEach((box) => {
   box.addEventListener("click", () => {
     if (turnO) {
-      //playerO
       box.innerText = "O";
       turnO = false;
-      
     } else {
-      //playerX
       box.innerText = "X";
       turnO = true;
     }
+
     box.disabled = true;
     count++;
 
     let isWinner = checkWinner();
 
-    if (count === 9 && !isWinner) {
+    // If no winner and all boxes are filled
+    if (!isWinner && count === 9) {
       gameDraw();
     }
   });
@@ -72,21 +71,20 @@ const showWinner = (winner) => {
   msgContainer.classList.remove("hide");
   disableBoxes();
 };
-
 const checkWinner = () => {
   for (let pattern of winPatterns) {
     let pos1Val = boxes[pattern[0]].innerText;
     let pos2Val = boxes[pattern[1]].innerText;
     let pos3Val = boxes[pattern[2]].innerText;
 
-    if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
+    if (pos1Val !== "" && pos2Val !== "" && pos3Val !== "") {
       if (pos1Val === pos2Val && pos2Val === pos3Val) {
         showWinner(pos1Val);
         return true;
       }
     }
   }
+  return false;   // Important
 };
-
 newGameBtn.addEventListener("click", resetGame);
 resetBtn.addEventListener("click", resetGame);
